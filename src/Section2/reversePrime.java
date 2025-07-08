cf0f8cbfe74a8f0ec58e2420ca6e4596a3a9bd36
2heunxun <seapeon@naver.com>
2025-07-08 17:52:14 +0900

    2-6 뒤집은 소수

--- src/Section2/reversePrime.java
+++ src/Section2/reversePrime.java
@@ -5,7 +5,6 @@ import java.util.*;
 //2-6뒤집은 소수
 public class reversePrime {
     Scanner scanner = new Scanner(System.in);
-    int[] list;
     int enter;
 
     void input(){
@@ -25,21 +24,31 @@ public class reversePrime {
 
     // 숫자를 뒤집는 함수
     public static int reverse(int num) {
-        int rev = 0;
-
-        while (num > 0) {
-            rev = rev * 10 + (num % 10);
+        int rev = 0; // 변수 초기화
+        while (num > 0) { // num 이 0 보다 클 때
+            rev = rev * 10 + (num % 10); //
             num /= 10;
         }
+        /*
+        만약 num이 492라면
+        rev = 0 * 10 + (492%10) =>  rev= 2
+        492/10 = 49
 
+        rev = 2*10 + (49%10) -> rev = 29
+        49/10 = 4
+
+        rev = 29*10 + (4%10) => rev = 294
+        2/10 = 0;
+
+         */
         return rev;
     }
 
 
     boolean isPrime(int num){
-        if(num < 2) return false;
+        if(num < 2) return false; // 2미만은 false 소수탈락
 
-        for(int i = 2; i <= Math.sqrt(num); i++){
+        for(int i = 2; i <= Math.sqrt(num); i++){ // 굳이 num 다 돌필요 없이 num루트 가지만 돌아도 됨
             if(num % i == 0) return false;
         }
         return true;
